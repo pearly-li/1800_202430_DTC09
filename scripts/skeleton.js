1a68e3a2e8d18f421eba63db2dd077b2d3f6c1d7
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-10-30 11:12:57 -0700

    Create alternate footer

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,10 +3,10 @@ function loadSkeleton() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer_after_login.html'));
         } else {
             console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer_before_login.html'));
         }
     });
 }
