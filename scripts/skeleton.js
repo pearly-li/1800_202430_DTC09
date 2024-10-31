5f78246c18f30111068eb612359c99d50666f043
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-10-31 11:17:30 -0700

    Change id from navbarPlaceholder to headerPlaceholder

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,10 +2,10 @@ function loadSkeleton() {
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+            console.log($('#headerPlaceholder').load('./text/nav_after_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer_after_login.html'));
         } else {
-            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#headerPlaceholder').load('./text/nav_before_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer_before_login.html'));
         }
     });
