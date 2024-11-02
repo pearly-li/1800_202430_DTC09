0b42cb658f5a40feb09a5e110577e2fc93d4b998
Zoey Lee <zoeylee2626@gmail.com>
2024-11-02 03:28:36 -0700

    Revise event database

--- scripts/event.js
+++ scripts/event.js
@@ -1,9 +1,12 @@
 function createEvent() {
     var eventInfo = db.collection("events");
+    var cv = document.getElementById("category");
 
     eventInfo.add({
         title: document.getElementById("title").value,
         description: document.getElementById("description").value,
+        category: cv.options[cv.selectedIndex].value,
+        scale: parseInt(document.getElementById("scale").value),
         location: document.getElementById("address").value,
         date: document.getElementById("date").value,
         time: document.getElementById("time").value,
