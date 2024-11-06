30137aedf35e79b24b997e1f6144e96aef8fa929
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-11-06 11:32:04 -0800

    Create template for event cards

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -4,6 +4,7 @@ function loadSkeleton() {
         if (user) {
             console.log($('#headerPlaceholder').load('./text/nav_after_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer_after_login.html'));
+            console.log($('#event_card_template').load('./text/event_card_template.html'));
         } else {
             console.log($('#headerPlaceholder').load('./text/nav_before_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer_before_login.html'));
