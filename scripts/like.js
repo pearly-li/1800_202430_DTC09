2d0ebda73a5a87409fca5316a310ff7ed9d9b457
Zoey Lee <zoeylee2626@gmail.com>
2024-11-08 14:09:36 -0800

    fix

--- scripts/like.js
+++ scripts/like.js
@@ -3,10 +3,13 @@ count = 1
 function fillLike(){
     if (count == 1) {
         document.getElementById('like').src = "./images/f_heart.png"
+        count --;
+    }
+    else {
+        document.getElementById('like').src = "./images/heart.png"
         count ++;
     }
-    else
-        document.getElementById('like').src = "./images/heart.png"
+    
 }
 
 like_btn = document.getElementById("likeBtn")
