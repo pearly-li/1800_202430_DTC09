34f19e31f6c95dd270561c04dd986e7c757e078b
Pearly Li <pearlyli@protonmail.com>
2024-11-15 19:04:30 -0800

    Disable console logs

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,34 +1,32 @@
 function getNameFromAuth() {
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid);
-            console.log(user.displayName);
-            userName = user.displayName;
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // console.log(user.uid);
+      // console.log(user.displayName);
+      userName = user.displayName;
 
-            document.getElementById("name_goes_here").innerText = userName;
-
-        } else {
-            console.log("No user is logged in");
-        }
-    });
+      document.getElementById("name_goes_here").innerText = userName;
+    } else {
+      console.log("No user is logged in");
+    }
+  });
 }
 getNameFromAuth();
 
 function getPageName() {
-    document.getElementById("page_label_goes_here").innerHTML = (document.getElementById("page_label")).innerHTML
+  document.getElementById("page_label_goes_here").innerHTML =
+    document.getElementById("page_label").innerHTML;
 }
 getPageName();
-
-
 
 var originalPagePosition = window.scrollY;
 
 window.onscroll = function hideNavbar() {
-    var currentPagePosition = window.scrollY;
-    if (originalPagePosition > currentPagePosition) {
-        document.getElementById("header_navbar").style.top = "0";
-    } else {
-        document.getElementById("header_navbar").style.top = "-75px";
-    }
-    originalPagePosition = currentPagePosition;
-}
+  var currentPagePosition = window.scrollY;
+  if (originalPagePosition > currentPagePosition) {
+    document.getElementById("header_navbar").style.top = "0";
+  } else {
+    document.getElementById("header_navbar").style.top = "-75px";
+  }
+  originalPagePosition = currentPagePosition;
+};
