b5408e7b525619a512f48e52287c6cc4edb3b2c0
Pearly Li <pearlyli@protonmail.com>
2024-11-16 17:04:31 -0800

    Add eventDocID to write_message.js

--- scripts/write_message.js
+++ scripts/write_message.js
@@ -1,18 +1,37 @@
+//go to the correct user document by referencing to the user uid
+var currentUser = db.collection("users").doc(user.uid);
+var userID = user.uid;
+function getEventID() {
+  eventID = db.collection("events").doc();
+}
+
+// function saveEventDocumentIDAndRedirect() {
+//   let params = new URL(window.location.href); //get the url from the search bar
+//   let ID = params.searchParams.get("docID");
+//   localStorage.setItem("eventDocID", ID);
+//   window.location.href = "message_list.html";
+// }
+// Go through all the messages, then do a loop looking for the reference to that event ID
+
 function writeMessage() {
   firebase.auth().onAuthStateChanged((user) => {
     // Check if user is signed in:
     if (user) {
       //go to the correct user document by referencing to the user uid
-      currentUser = db.collection("users").doc(user.uid);
       //get the document for current user.
       currentUser.get().then((userDoc) => {
         //get the data fields of the user
         let userName = userDoc.data().name;
         var messageRef = db.collection("messages");
-
+        let reviewerName = userName;
+        let eventDocID = db.collection("events").doc.id;
+        console.log(eventDocID);
+        // var eventID = eventInfo.data().eventID;
         messageRef
           .add({
-            reviewer_name: userName,
+            userID: userID,
+            eventDocID: eventDocID,
+            reviewer_name: reviewerName,
             message_description:
               document.getElementById("messageDescription").value,
             message_created_at: firebase.firestore.FieldValue.serverTimestamp(),
