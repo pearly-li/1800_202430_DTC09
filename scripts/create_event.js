66cbe9386f0fe34545490fbae82b4c61c28b2a56
Pearly Li <pearlyli@protonmail.com>
2024-11-17 00:25:04 -0800

    Format create_event.js

--- scripts/create_event.js
+++ scripts/create_event.js
@@ -3,101 +3,111 @@ var image;
 var title;
 var dateTime;
 
-
 function listenFileSelect() {
-    //listen for file selection
-    var fileInput = document.getElementById("image");
-    const image = document.getElementById("user_pic");
+  //listen for file selection
+  var fileInput = document.getElementById("image");
+  const image = document.getElementById("user_pic");
 
-    //when a change happens to the File Chooser Input
-    fileInput.addEventListener('change', function (e) {
-        ImageFile = e.target.files[0];
-        var blob = URL.createObjectURL(ImageFile);
-        image.src = blob; //Display this image
-    })
+  //when a change happens to the File Chooser Input
+  fileInput.addEventListener("change", function (e) {
+    ImageFile = e.target.files[0];
+    var blob = URL.createObjectURL(ImageFile);
+    image.src = blob; //Display this image
+  });
 }
 listenFileSelect();
 
 function createEvent() {
-    var eventInfo = db.collection("events");
-    var category = document.getElementById("category");
-    title = document.getElementById("title").value;
-    dateTime = document.getElementById("dateTime").value,
-
+  var eventInfo = db.collection("events");
+  var category = document.getElementById("category");
+  title = document.getElementById("title").value;
+  (dateTime = document.getElementById("dateTime").value),
     firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            eventInfo.add({
-                host: user.uid,
-                title: title,
-                description: document.getElementById("description").value,
-                category: category.options[category.selectedIndex].value,
-                scale: parseInt(document.getElementById("scale").value),
-                location: document.getElementById("address").value,
-                dateTime: dateTime,
-                last_updated: firebase.firestore.FieldValue.serverTimestamp()
-            }) .then (doc => {
-                console.log("1. Post document added!");
-                console.log(doc.id);
-                uploadPic(doc.id);
-            })
-        } else {
-            console.log("Error, no user signed in");
-        }
+      if (user) {
+        eventInfo
+          .add({
+            host: user.uid,
+            title: title,
+            description: document.getElementById("description").value,
+            category: category.options[category.selectedIndex].value,
+            scale: parseInt(document.getElementById("scale").value),
+            location: document.getElementById("address").value,
+            dateTime: dateTime,
+            last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+          })
+          .then((doc) => {
+            console.log("1. Post document added!");
+            console.log(doc.id);
+            uploadPic(doc.id);
+          });
+      } else {
+        console.log("Error, no user signed in");
+      }
     });
 }
 
-create_event_btn = document.getElementById("create_event_btn")
+create_event_btn = document.getElementById("create_event_btn");
 create_event_btn.addEventListener("click", () => {
-    createEvent()
-})
+  createEvent();
+});
 
 function uploadPic(postDocID) {
-    console.log("inside uploadPic " + postDocID);
-    var storageRef = storage.ref("images/" + postDocID + ".jpg");
+  console.log("inside uploadPic " + postDocID);
+  var storageRef = storage.ref("images/" + postDocID + ".jpg");
 
-    storageRef.put(ImageFile)
+  storageRef
+    .put(ImageFile)
 
-        .then(function () {
-            console.log('2. Uploaded to Cloud Storage.');
-            storageRef.getDownloadURL()
+    .then(function () {
+      console.log("2. Uploaded to Cloud Storage.");
+      storageRef
+        .getDownloadURL()
 
-                .then(function (url) {
-                    console.log("3. Got the download URL.");
-                    image = url;
-                    db.collection("events").doc(postDocID).update({
-                        "image": url
-                    })
-                        .then(function () {
-                            console.log('4. Added pic URL to Firestore.');
-                            savePostInfoforUser(postDocID);
-                        })
-                })
-        })
-        .catch((error) => {
-            console.log("error uploading to cloud storage");
-        })
+        .then(function (url) {
+          console.log("3. Got the download URL.");
+          image = url;
+          db.collection("events")
+            .doc(postDocID)
+            .update({
+              image: url,
+            })
+            .then(function () {
+              console.log("4. Added pic URL to Firestore.");
+              savePostInfoforUser(postDocID);
+            });
+        });
+    })
+    .catch((error) => {
+      console.log("error uploading to cloud storage");
+    });
 }
 
 //saves the post information for the user, in an array
 function savePostInfoforUser(postDocID) {
-    firebase.auth().onAuthStateChanged(user => {
-        console.log("user id is: " + user.uid);
-        console.log("postdoc id is: " + postDocID);
-        db.collection("users").doc(user.uid).update({
-            myposts: firebase.firestore.FieldValue.arrayUnion(postDocID)
-        })
-        db.collection("events").doc(postDocID).update({
-            participants: firebase.firestore.FieldValue.arrayUnion(user.uid)
-        })
-        db.collection("users").doc(user.uid).update({
-            eventAttend: firebase.firestore.FieldValue.arrayUnion(postDocID)
-        })
-            .then(() => {
-                console.log("5. Saved to user's document!");
-                window.location.href = "event_detail.html?docID=" + postDocID
-            })
-            .catch((error) => {
-                console.error("Error writing document: ", error);
-            });
-    })
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log("user id is: " + user.uid);
+    console.log("postdoc id is: " + postDocID);
+    db.collection("users")
+      .doc(user.uid)
+      .update({
+        myposts: firebase.firestore.FieldValue.arrayUnion(postDocID),
+      });
+    db.collection("events")
+      .doc(postDocID)
+      .update({
+        participants: firebase.firestore.FieldValue.arrayUnion(user.uid),
+      });
+    db.collection("users")
+      .doc(user.uid)
+      .update({
+        eventAttend: firebase.firestore.FieldValue.arrayUnion(postDocID),
+      })
+      .then(() => {
+        console.log("5. Saved to user's document!");
+        window.location.href = "event_detail.html?docID=" + postDocID;
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  });
 }
