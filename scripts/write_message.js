9ba2db041f23675e81c12312f074834b2eb6d865
Pearly Li <pearlyli@protonmail.com>
2024-11-17 00:22:35 -0800

    Add event ID and reviewer profile picture to write_message.js

--- scripts/write_message.js
+++ scripts/write_message.js
@@ -1,10 +1,9 @@
 //go to the correct user document by referencing to the user uid
-var currentUser = db.collection("users").doc(user.uid);
-var userID = user.uid;
-function getEventID() {
-  eventID = db.collection("events").doc();
-}
+var params = new URL(window.location.href);
+var eventID = params.searchParams.get("docID");
 
+// When you run into a problem, use the debugging and set a breakpoint right after the thing I need to test. We tested the doc and we could fetch data.
+// After we set breakpoint at
 // function saveEventDocumentIDAndRedirect() {
 //   let params = new URL(window.location.href); //get the url from the search bar
 //   let ID = params.searchParams.get("docID");
@@ -19,18 +18,22 @@ function writeMessage() {
     if (user) {
       //go to the correct user document by referencing to the user uid
       //get the document for current user.
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
       currentUser.get().then((userDoc) => {
         //get the data fields of the user
         let userName = userDoc.data().name;
         var messageRef = db.collection("messages");
         let reviewerName = userName;
-        let eventDocID = db.collection("events").doc.id;
-        console.log(eventDocID);
+        let reviewer_profile_picture = userDoc.data().profile_picture;
+        // let eventDocID = db.collection("events").doc.id;
+        console.log(eventID);
         // var eventID = eventInfo.data().eventID;
         messageRef
           .add({
+            reviewer_profile_picture: reviewer_profile_picture,
             userID: userID,
-            eventDocID: eventDocID,
+            eventID: eventID,
             reviewer_name: reviewerName,
             message_description:
               document.getElementById("messageDescription").value,
@@ -53,3 +56,6 @@ function writeMessage() {
   });
 }
 // Note to self: Will add a separate function to edit messages later, which will update the last_updated field but NOT the message_created_at
+function addMessageToMessageList() {
+  window.location.href = "message_list.html";
+}
