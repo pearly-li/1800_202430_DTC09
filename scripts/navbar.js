d7118038fcbc091a1198d7cc9f12c98ba31d0497
Pearly Li <pearlyli@protonmail.com>
2024-11-17 22:58:16 -0800

    Disable getNameFromAuth in navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,17 +1,17 @@
-function getNameFromAuth() {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      // console.log(user.uid);
-      // console.log(user.displayName);
-      userName = user.displayName;
+// function getNameFromAuth() {
+//   firebase.auth().onAuthStateChanged((user) => {
+//     if (user) {
+//       // console.log(user.uid);
+//       // console.log(user.displayName);
+//       userName = user.displayName;
 
-      document.getElementById("name_goes_here").innerText = userName;
-    } else {
-      console.log("No user is logged in");
-    }
-  });
-}
-getNameFromAuth();
+//       document.getElementById("name_goes_here").innerText = userName;
+//     } else {
+//       console.log("No user is logged in");
+//     }
+//   });
+// }
+// getNameFromAuth();
 
 function getPageName() {
   document.getElementById("page_label_goes_here").innerHTML =
