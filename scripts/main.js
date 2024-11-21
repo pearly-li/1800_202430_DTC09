fa94740450964a3dda45f032d2996336d8658af4
Kanon Nishiyama <knishiyama@my.bcit.ca>
2024-11-21 11:27:01 -0800

    Remove commented code

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,3 @@
-// function insertNameFromFirestore() {
-//     // Check if the user is logged in:
-//     firebase.auth().onAuthStateChanged(user => {
-//         if (user) {
-//             console.log(user.uid); // Let's know who the logged-in user is by logging their UID
-//             currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
-//             currentUser.get().then(userDoc => {
-//                 // Get the user name
-//                 let userName = userDoc.data().name;
-//                 console.log(userName);
-//                 //$("#name-goes-here").text(userName); // jQuery
-//                 document.getElementById("name-goes-here").innerText = userName;
-//             })
-//         } else {
-//             console.log("No user is logged in."); // Log a message when no user is logged in
-//         }
-//     })
-// }
-// insertNameFromFirestore();
-
 function doAll() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -72,6 +52,7 @@ displayUpcomingEventCards()
 function displayUserSchedule(user) {
     var cardTemplate = document.getElementById("user_event_schedule_template");
     var eventIDs = []
+    
 
     db.collection("users").doc(user.uid)
         .get()
