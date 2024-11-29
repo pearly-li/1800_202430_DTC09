05063c3d90a3a2c81dc3c09215ccef3738b025be
Pearly Li <pearlyli@protonmail.com>
2024-11-28 17:21:11 -0800

    Combine profile_picture_only into navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -32,8 +32,38 @@ window.onscroll = function hideNavbar() {
   originalPagePosition = currentPagePosition;
 };
 
-if (document.getElementById("page_label_goes_here").innerText == "Social Scout") {
-  btn = document.getElementById("back_arrow_btn")
-  btn.classList.add("invisible")
+if (
+  document.getElementById("page_label_goes_here").innerText == "Social Scout"
+) {
+  btn = document.getElementById("back_arrow_btn");
+  btn.classList.add("invisible");
   btn.disabled = true;
 }
+
+// This is the function that generates profile_picture in the top nav
+var currentUser; //points to the document of the user who is logged in
+
+function populateUserInfo() {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if user is signed in:
+    if (user) {
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid);
+      //get the document for current user.
+      currentUser.get().then((userDoc) => {
+        let userProfilePicture = userDoc.data().profile_picture;
+        // Put the profile picture in the header
+        if (userProfilePicture) {
+          document.querySelector(
+            "#pictureGoesHere"
+          ).src = `images/${userProfilePicture}`;
+        }
+      });
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
+  });
+}
+//call the function to run it
+populateUserInfo();
