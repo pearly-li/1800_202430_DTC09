1aab76048b6f3f61ceb603f56dcf691f8df680c3
brandonlma <blam74@my.bcit.ca>
2024-11-29 14:30:02 -0800

    Remove unnecessary code and change location format

--- scripts/create_event.js
+++ scripts/create_event.js
@@ -57,6 +57,7 @@ function createEvent() {
             streetNumber: document.getElementById("streetNumber").value,
             streetName: document.getElementById("streetName").value,
             city: document.getElementById("city").value,
+            // location: document.getElementById("streetNumber").value + " " + document.getElementById("streetName").value + " " + document.getElementById("city").value,
             dateTime: dateTime,
             last_updated: firebase.firestore.FieldValue.serverTimestamp(),
           })
@@ -174,6 +175,7 @@ function editEvent(docID) {
             streetNumber: document.getElementById("streetNumber").value,
             streetName: document.getElementById("streetName").value,
             city: document.getElementById("city").value,
+            location: document.getElementById("streetNumber").value + " " + document.getElementById("streetName").value + " " + document.getElementById("city").value,
             dateTime: dateTime,
             last_updated: firebase.firestore.FieldValue.serverTimestamp(),
           })
@@ -186,61 +188,4 @@ function editEvent(docID) {
         console.log("Error, no user signed in");
       }
     });
-}
-
-function getLocation() {
-  map;
-  originPlaceId;
-  destinationPlaceId;
-  travelMode;
-  directionsService;
-  directionsRenderer;
-  this.originPlaceId = "";
-  this.destinationPlaceId = "";
-  this.travelMode = google.maps.TravelMode.WALKING;
-  this.directionsService = new google.maps.DirectionsService();
-  this.directionsRenderer = new google.maps.DirectionsRenderer();
-  this.directionsRenderer.setMap(map);
-
-  const originInput = document.getElementById("origin-input");
-  const destinationInput = document.getElementById("destination-input");
-  const modeSelector = document.getElementById("mode-selector");
-  // Specify just the place data fields that you need.
-  const originAutocomplete = new google.maps.places.Autocomplete(originInput, {
-    fields: ["place_id"],
-  });
-  // Specify just the place data fields that you need.
-  const destinationAutocomplete = new google.maps.places.Autocomplete(
-    destinationInput,
-    { fields: ["place_id"] }
-  );
-
-  this.setupClickListener("changemode-walking", google.maps.TravelMode.WALKING);
-  this.setupClickListener("changemode-transit", google.maps.TravelMode.TRANSIT);
-  this.setupClickListener("changemode-driving", google.maps.TravelMode.DRIVING);
-  this.setupPlaceChangedListener(originAutocomplete, "ORIG");
-  this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-    destinationInput
-  );
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-  setupPlaceChangedListener(autocomplete, mode);
-  autocomplete.bindTo("bounds", this.map);
-  autocomplete.addListener("place_changed", () => {
-    const place = autocomplete.getPlace();
-
-    if (!place.place_id) {
-      window.alert("Please select an option from the dropdown list.");
-      return;
-    }
-
-    if (mode === "ORIG") {
-      this.originPlaceId = place.place_id;
-    } else {
-      this.destinationPlaceId = place.place_id;
-    }
-
-    this.route();
-  });
 }
